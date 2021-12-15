5466a1085c33a7b082099c4bd66d81da554d3c95
Daniel <daniel_kansaon@hotmail.com>
2021-12-15 16:58:07 -0300

    ajustando header e sincronizando com o servidor

--- brazil/header.php
+++ brazil/header.php
@@ -7,7 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    
+    <link rel="shortcut icon" href="http://www.whatsapp-monitor.dcc.ufmg.br/indonesia/img/favicon-32-32.png" type="image/x-icon"/>
+        
     <title>Monitor de WhatsApp</title>
     
     <!-- Bootstrap core CSS -->
